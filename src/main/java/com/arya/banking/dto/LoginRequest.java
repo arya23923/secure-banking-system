132f6d576ea8af803548c0d0dc43444538dc58d6
Arya Gupta <aryagupta239@gmail.com>
2026-02-19 22:05:06 +0530

    Login request dto

--- src/main/java/com/arya/banking/dto/LoginRequest.java
+++ src/main/java/com/arya/banking/dto/LoginRequest.java
@@ -1,5 +1,10 @@
 package com.arya.banking.dto;
 
-public class LoginRequest {
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter @Setter
+public class LoginRequest {
+    private String email;
+    private String password;
 }

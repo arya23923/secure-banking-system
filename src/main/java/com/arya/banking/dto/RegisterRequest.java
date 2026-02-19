8d28f77d71dc5a89d5e96f8433154076b82d9eff
Arya Gupta <aryagupta239@gmail.com>
2026-02-19 22:04:12 +0530

    data transfer object for secure banking

--- src/main/java/com/arya/banking/dto/RegisterRequest.java
+++ src/main/java/com/arya/banking/dto/RegisterRequest.java
@@ -1,5 +1,10 @@
 package com.arya.banking.dto;
 
-public class RegisterRequest {
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter @Setter
+public class RegisterRequest {
+    private String email;
+    private String password;
 }

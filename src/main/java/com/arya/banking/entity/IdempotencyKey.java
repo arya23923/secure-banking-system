3f4e3e2e68b6e8e81e7ce870bfb0d9c63954e7bf
Arya Gupta <aryagupta239@gmail.com>
2026-02-19 21:39:54 +0530

    Initialize idempotency keys for prevention of double payments

--- src/main/java/com/arya/banking/entity/IdempotencyKey.java
+++ src/main/java/com/arya/banking/entity/IdempotencyKey.java
@@ -1,5 +1,24 @@
 package com.arya.banking.entity;
 
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "idempotency_keys")
+@Getter @Setter
 public class IdempotencyKey {
 
+    @Id
+    private String key;
+
+    @Lob
+    private String response;
+
+    private LocalDateTime createdAt = LocalDateTime.now();
 }

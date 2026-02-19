0568593e9393fea03e314c76dab3ecd6e003bb2c
Arya Gupta <aryagupta239@gmail.com>
2026-02-19 22:27:16 +0530

    User authorization controller

--- src/main/java/com/arya/banking/controller/AuthController.java
+++ src/main/java/com/arya/banking/controller/AuthController.java
@@ -1,5 +1,29 @@
 package com.arya.banking.controller;
 
-public class AuthController {
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.arya.banking.dto.LoginRequest;
+import com.arya.banking.dto.RegisterRequest;
+import com.arya.banking.service.AuthService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class AuthController {
+    private final AuthService authService;
+
+    @PostMapping("/register")
+    public String register(@RequestBody RegisterRequest request) {
+        return authService.register(request);
+    }
+
+    @PostMapping("/login")
+    public String login(@RequestBody LoginRequest request) {
+        return authService.login(request);
+    }
 }

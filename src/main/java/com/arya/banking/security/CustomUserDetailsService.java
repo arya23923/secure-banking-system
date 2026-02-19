212325ebc922343217f53ccaff32adf191f3ad9a
Arya Gupta <aryagupta239@gmail.com>
2026-02-19 22:01:50 +0530

    Configure spring security

--- src/main/java/com/arya/banking/security/CustomUserDetailsService.java
+++ src/main/java/com/arya/banking/security/CustomUserDetailsService.java
@@ -1,5 +1,30 @@
 package com.arya.banking.security;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableMethodSecurity
 public class CustomUserDetailsService {
 
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/auth/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .build();
+    }
 }

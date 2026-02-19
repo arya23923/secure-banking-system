bbc97e5d5e9235df43012a950b2c99f9405d64d2
Arya Gupta <aryagupta239@gmail.com>
2026-02-19 21:55:46 +0530

    jwt security encryption addition

--- src/main/java/com/arya/banking/security/JwtService.java
+++ src/main/java/com/arya/banking/security/JwtService.java
@@ -1,5 +1,28 @@
 package com.arya.banking.security;
 
-public class JwtService {
+import org.springframework.stereotype.Component;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import java.util.Date;
 
+@Component
+public class JwtService {
+    private final String SECRET = "superSecretKey";
+
+    public String generateToken(String email) {
+        return Jwts.builder()
+                .setSubject(email)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + 86400000))
+                .signWith(SignatureAlgorithm.HS256, SECRET)
+                .compact();
+    }
+
+    public String extractUsername(String token) {
+        return Jwts.parser()
+                .setSigningKey(SECRET)
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
 }
